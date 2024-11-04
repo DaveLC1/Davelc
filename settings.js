dfd1d5de0c7f9218268931f56ad910bebcce6385
DaveLC1 <144999652+DaveLC1@users.noreply.github.com>
2024-11-04 02:13:31 +0100

    settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'My Sticker Pack',
   author: 'My Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "DaveLC🕊️",
+  botOwner: 'Dave', // Your name
+  ownerNumber: '+2347049383110', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
